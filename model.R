3ff89c274688fe2cd5dc0b0b70def0bf12a6d72c
Lucien Bastin <lucien.bastin@protonmail.com>
2025-10-13 18:25:35 +0200

    Add NPZD equations

--- model.R
+++ model.R
@@ -10,6 +10,10 @@ theta <- function(t) {
   y > 0
 }
 
+A_mix <- function(t) {
+  theta(-t) * a
+}
+
 growth_rate <- function(t, vars) {
   r * theta(t) * (vars$nutrients**2 / (alpha**2 + vars$nutrients**2))
 }
@@ -19,19 +23,42 @@ grazing <- function(t, vars) {
 }
 
 nutrients <- function(t, vars) {
-  -vars$R * vars$phyto
+  -vars$R * vars$phyto *
+    vars$L_ZN * (vars$zoo - Z_0) +
+    vars$L_PN * (vars$phyto - P_0) +
+    L_D * vars$detritus +
+    A_mix(t) * (vars$detritus - vars$nutrients) +
+    SN_ext
 }
 
 phyto <- function(t, vars) {
-  0.001
+  vars$R * vars$phyto - vars$L_P * (vars$phyto - P_0) - vars$G * vars$zoo
 }
 
 zoo <- function(t, vars) {
-  0.001
+  vars$G * vars$zoo - vars$L_Z * (vars$zoo - Z_0)
+}
+
+detritus <- function(t, vars) {
+  vars$L_ZD * (vars$zoo - Z_0) +
+    vars$L_PD * (vars$phyto - P_0) -
+    L_D * vars$detritus - A_mix(t) * (vars$detritus - vars$nutrients) + SD_ext
+}
+
+L_PN <- function(t) {
+  theta(t) * eta + theta(-t) * lambda
+}
+
+L_PD <- function(t) {
+  theta(t) * delta + theta(-t) * lambda
 }
 
 L_ZN <- function(t, vars) {
-  theta(t) * sigma + physical_control(-t) * gamma
+  theta(t) * nu + theta(-t) * gamma
+}
+
+L_ZD <- function(t, vars) {
+  theta(t) * sigma + theta(-t) * gamma
 }
 
 # Model parameters -------------------------------------------------------
@@ -49,6 +76,22 @@ r <- 1
 alpha <- 0.1
 I <- 1.2
 g <- 0.5
+a <- 0.2
+
+gamma <- 0.05
+delta <- 0.02
+eta <- 0.01
+lambda <- 0.05
+nu <- 0.01
+sigma <- 0.02
+
+Z_0 <- 0.01
+P_0 <- 0.01
+
+L_D <- 0
+
+SN_ext <- 0
+SD_ext <- 0
 
 # Initialize tracked variables -------------------------------------------
 zeros <- rep(0, length(time_seq))
@@ -57,15 +100,23 @@ vars <- data.frame(
   "time" = seq(t_0, t_f, dt),
   "theta" = zeros,
   "R" = zeros,
+  "G" = zeros,
+  "L_PN" = zeros,
+  "L_PD" = zeros,
+  "L_ZN" = zeros,
+  "L_ZD" = zeros,
+  "L_Z" = zeros,
+  "L_D" = zeros,
+  "L_P" = zeros,
   "nutrients" = zeros,
   "zoo" = zeros,
   "phyto" = zeros,
   "detritus" = zeros
 )
 
-vars$nutrients[1] <- 0
-vars$zoo[1] <- 1
-vars$phyto[1] <- 1
+vars$nutrients[1] <- 1
+vars$zoo[1] <- Z_0
+vars$phyto[1] <- P_0
 vars$detritus[1] <- 1
 
 # Main model loop --------------------------------------------------------
@@ -78,21 +129,39 @@ for (i in 2:length(time_seq)) {
   vars$R[i] <- growth_rate(t, vars_before)
   vars$G[i] <- grazing(t, vars_before)
 
+  vars$L_PN[i] <- L_PN(t)
+  vars$L_PD[i] <- L_PD(t)
+  vars$L_ZN[i] <- L_ZN(t)
+  vars$L_ZD[i] <- L_ZD(t)
+
+  vars$L_Z[i] <- vars$L_ZN[i] + vars$L_ZD[i]
+  vars$L_P[i] <- vars$L_PN[i] + vars$L_PD[i]
+
   dNdt <- nutrients(t, vars_before)
   dZdt <- zoo(t, vars_before)
   dPdt <- phyto(t, vars_before)
+  dDdt <- detritus(t, vars_before)
 
   vars$nutrients[i] <- vars$nutrients[i - 1] + dNdt * dt
   vars$zoo[i] <- vars$zoo[i - 1] + dZdt * dt
   vars$phyto[i] <- vars$phyto[i - 1] + dPdt * dt
-  # vars$detritus[i] <- vars$detritus[i - 1] + detritus(t) * dt
+  vars$detritus[i] <- vars$detritus[i - 1] + dDdt * dt
 }
 
 # Plot results -----------------------------------------------------------
 
-par(mfrow = c(2, 2))
-plot(vars$time, vars$theta, type = "l", col = "black")
-plot(vars$time, vars$nutrients, type = "l", col = "red")
+par(mfrow = c(3, 2))
 
+plot(vars$time, vars$theta, type = "l", col = "black")
 plot(vars$time, vars$R, type = "l", col = "black")
 plot(vars$time, vars$G, type = "l", col = "black")
+
+plot(vars$time, vars$nutrients, type = "l", col = "red", ylim = c(0, 2))
+lines(vars$time, vars$zoo, type = "l", col = "blue")
+lines(vars$time, vars$phyto, type = "l", col = "green")
+lines(vars$time, vars$detritus, type = "l", col = "yellow")
+
+plot(vars$time, vars$L_PN, type = "l")
+lines(vars$time, vars$L_PD, type = "l")
+lines(vars$time, vars$L_ZN, type = "l")
+lines(vars$time, vars$L_ZD, type = "l")
