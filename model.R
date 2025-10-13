c8a03a4f802e54f233937b381e647a4eb35ccacc
Lucien Bastin <lucien.bastin@protonmail.com>
2025-10-13 17:03:20 +0200

    Add growth rate and grazing

--- model.R
+++ model.R
@@ -11,11 +11,23 @@ physical_control <- function(t) {
 }
 
 growth_rate <- function(t, vars) {
-  r * vars$theta
+  r * vars$theta * (vars$nutrients**2 / (alpha**2 + vars$nutrients**2))
+}
+
+grazing <- function(t, vars) {
+  g * vars$theta * (1 - exp(-I * vars$phytoplankton**2))
 }
 
 nutrients <- function(t) {
-  1
+  0.001
+}
+
+phytoplankton <- function(t) {
+  0.001
+}
+
+zooplankton <- function(t) {
+  0.001
 }
 
 # Model parameters -------------------------------------------------------
@@ -30,6 +42,9 @@ time_seq <- seq(t_0, t_f, dt)
 
 T_period <- 365
 r <- 1
+alpha <- 0.1
+I <- 1.2
+g <- 0.5
 
 # Initialize tracked variables -------------------------------------------
 zeros <- rep(0, length(time_seq))
@@ -44,7 +59,7 @@ vars <- data.frame(
   "detritus" = zeros
 )
 
-vars$nutrients[1] <- 1
+vars$nutrients[1] <- 0
 vars$zooplankton[1] <- 1
 vars$phytoplankton[1] <- 1
 vars$detritus[1] <- 1
@@ -57,12 +72,15 @@ for (i in 2:length(time_seq)) {
 
   vars$theta[i] <- physical_control(t)
   vars$R[i] <- growth_rate(t, vars_before)
-  
+  vars$G[i] <- grazing(t, vars_before)
+
   dNdt <- nutrients(t)
+  dZdt <- zooplankton(t)
+  dPdt <- phytoplankton(t)
 
   vars$nutrients[i] <- vars$nutrients[i - 1] + nutrients(t) * dt
-  # vars$zooplankton[i] <- vars$zooplankton[i - 1] + zooplankton(t) * dt
-  # vars$phytoplankton[i] <- vars$phytoplankton[i - 1] + phytoplankton(t) * dt
+  vars$zooplankton[i] <- vars$zooplankton[i - 1] + zooplankton(t) * dt
+  vars$phytoplankton[i] <- vars$phytoplankton[i - 1] + phytoplankton(t) * dt
   # vars$detritus[i] <- vars$detritus[i - 1] + detritus(t) * dt
 }
 
@@ -73,3 +91,4 @@ plot(vars$time, vars$theta, type = "l", col = "black")
 plot(vars$time, vars$nutrients, type = "l", col = "red")
 
 plot(vars$time, vars$R, type = "l", col = "black")
+plot(vars$time, vars$G, type = "l", col = "black")
