138c90d0a89e0647cafb97d6853ddf147d599873
Lucien Bastin <lucien.bastin@protonmail.com>
2025-10-16 16:07:12 +0200

    Add functions and time-dependant sinking rate

--- model.R
+++ model.R
@@ -4,6 +4,15 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 
+# Shape palette ----------------------------------------------------------
+
+state_vars_palette <- c(
+  "nutrients" = "solid",
+  "zoo" = "dotted",
+  "phyto" = "dashed",
+  "detritus" = "twodash"
+)
+
 # Functions --------------------------------------------------------------
 
 y <- function(t) {
@@ -57,33 +66,33 @@ L_PN <- function(t) {
   theta(y(t)) * eta + theta(-y(t)) * lambda
 }
 
-L_PD <- function(t) {
-  theta(y(t)) * delta + theta(-y(t)) * lambda
+L_PD <- function(t, vars) {
+  theta(y(t)) * vars$delta + theta(-y(t)) * lambda
 }
 
-L_ZN <- function(t, vars) {
+L_ZN <- function(t) {
   theta(y(t)) * nu + theta(-y(t)) * gamma
 }
 
-L_ZD <- function(t, vars) {
+L_ZD <- function(t) {
   theta(y(t)) * sigma + theta(-y(t)) * gamma
 }
 
-# Model parameters -------------------------------------------------------
+sinking_coef <- function(t, type) {
+  if (type == 0) {
+    r <- delta
+  } else if (type == 1) {
+    r <- delta_1 * exp(-((t - T_s)**2 / C_s))
+  } else {
+    r <- delta_2 * 0.5 * (1 + cos((2 * pi * t / T_period) - (pi / 2)))
+  }
 
-t_0 <- 0
-t_f <- 365
-dt <- 0.05
+  r
+}
 
-time_seq <- seq(t_0, t_f, dt)
-
-pb <- txtProgressBar(
-  min = 0,
-  max = length(time_seq),
-  style = 3,
-  width = 50,
-  char = "="
-)
+sinking_rate <- function(t, vars) {
+  theta(y(t)) * vars$delta
+}
 
 # Constants --------------------------------------------------------------
 
@@ -96,6 +105,8 @@ a <- 0.2
 
 gamma <- 0.05
 delta <- 0.02
+delta_1 <- 0.035
+delta_2 <- 0.02
 eta <- 0.01
 lambda <- 0.05
 nu <- 0.01
@@ -109,104 +120,141 @@ L_D <- 0
 SN_ext <- 0
 SD_ext <- 0
 
+T_s <- 93
+C_s <- 3600
+
 # Initialize tracked variables -------------------------------------------
-zeros <- rep(0, length(time_seq))
 
-vars <- data.frame(
-  "time" = seq(t_0, t_f, dt),
-  "y" = zeros,
-  "theta" = zeros,
-  "R" = zeros,
-  "G" = zeros,
-  "L_PN" = zeros,
-  "L_PD" = zeros,
-  "L_ZN" = zeros,
-  "L_ZD" = zeros,
-  "L_Z" = zeros,
-  "L_D" = zeros,
-  "L_P" = zeros,
-  "nutrients" = zeros,
-  "zoo" = zeros,
-  "phyto" = zeros,
-  "detritus" = zeros
-)
+init_vars <- function(length, dt) {
+  t_0 <- 0
+  t_f <- length
+  dt <- dt
 
-vars$nutrients[1] <- 0.99
-vars$zoo[1] <- Z_0
-vars$phyto[1] <- P_0
-vars$detritus[1] <- 0.99
+  time_seq <- seq(t_0, t_f, dt)
 
-# Main model loop --------------------------------------------------------
+  zeros <- rep(0, length(time_seq))
 
-for (i in 2:length(time_seq)) {
-  t <- vars$time[i]
-  vars_before <- vars[i - 1, ]
+  vars <- data.frame(
+    "time" = time_seq,
+    "y" = zeros,
+    "theta" = zeros,
+    "R" = zeros,
+    "G" = zeros,
+    "delta" = zeros,
+    "sinking_rate" = zeros,
+    "L_PN" = zeros,
+    "L_PD" = zeros,
+    "L_ZN" = zeros,
+    "L_ZD" = zeros,
+    "L_Z" = zeros,
+    "L_D" = zeros,
+    "L_P" = zeros,
+    "nutrients" = zeros,
+    "zoo" = zeros,
+    "phyto" = zeros,
+    "detritus" = zeros
+  )
 
-  vars$theta[i] <- theta(y(t))
-  vars$R[i] <- growth_rate(t, vars_before)
-  vars$G[i] <- grazing(t, vars_before)
+  vars$nutrients[1] <- 0.99
+  vars$zoo[1] <- Z_0
+  vars$phyto[1] <- P_0
+  vars$detritus[1] <- 0.99
 
-  vars$L_PN[i] <- L_PN(t)
-  vars$L_PD[i] <- L_PD(t)
-  vars$L_ZN[i] <- L_ZN(t)
-  vars$L_ZD[i] <- L_ZD(t)
-
-  vars$L_Z[i] <- vars$L_ZN[i] + vars$L_ZD[i]
-  vars$L_P[i] <- vars$L_PN[i] + vars$L_PD[i]
-
-  # K1 <- compute_state_vars(t, vars_before)
-  # K2 <- compute_state_vars(t + dt / 2, vars_before + dt / 2 * K1)
-  # K3 <- compute_state_vars(t + dt / 2, vars_before + dt / 2 * K2)
-  # K4 <- compute_state_vars(t + dt, vars_before + dt * K3)
-
-  # K <- compute_state_vars(t, vars_before) + (dt / 6) * (K1 + 2 * K2 + 2 * K3 + K4)
-
-  K <- compute_state_vars(t, vars_before)
-
-  dNdt <- K[1]
-  dZdt <- K[2]
-  dPdt <- K[3]
-  dDdt <- K[4]
-
-  # dNdt <- 0
-  # dZdt <- 0
-  # dPdt <- 0
-  # dDdt <- 0
-
-  vars$nutrients[i] <- max(vars$nutrients[i - 1] + dNdt * dt, 0)
-  vars$zoo[i] <- max(vars$zoo[i - 1] + dZdt * dt, 0)
-  vars$phyto[i] <- max(vars$phyto[i - 1] + dPdt * dt, 0)
-  vars$detritus[i] <- max(vars$detritus[i - 1] + dDdt * dt, 0)
-
-  setTxtProgressBar(pb, i)
+  vars
 }
 
-close(pb)
+# Main model loop function --------------------------------------------------------
+
+run_model <- function(dt, vars, sinking_rate_type) {
+  time_seq <- vars$time
+
+  pb <- txtProgressBar(
+    min = 0,
+    max = length(time_seq),
+    style = 3,
+    width = 50,
+    char = "="
+  )
+  
+  for (i in 2:length(time_seq)) {
+    t <- vars$time[i]
+    vars_before <- vars[i - 1, ]
+
+    vars$theta[i] <- theta(y(t))
+    vars$R[i] <- growth_rate(t, vars_before)
+    vars$G[i] <- grazing(t, vars_before)
+
+    vars$L_PN[i] <- L_PN(t)
+    vars$L_PD[i] <- L_PD(t, vars_before)
+    vars$L_ZN[i] <- L_ZN(t)
+    vars$L_ZD[i] <- L_ZD(t)
+
+    vars$L_Z[i] <- vars$L_ZN[i] + vars$L_ZD[i]
+    vars$L_P[i] <- vars$L_PN[i] + vars$L_PD[i]
+
+    vars$delta[i] <- sinking_coef(t, sinking_rate_type)
+    vars$sinking_rate[i] <- sinking_rate(t, vars_before)
+
+    K <- compute_state_vars(t, vars_before)
+
+    dNdt <- K[1]
+    dZdt <- K[2]
+    dPdt <- K[3]
+    dDdt <- K[4]
+
+    vars$nutrients[i] <- max(vars$nutrients[i - 1] + dNdt * dt, 0)
+    vars$zoo[i] <- max(vars$zoo[i - 1] + dZdt * dt, 0)
+    vars$phyto[i] <- max(vars$phyto[i - 1] + dPdt * dt, 0)
+    vars$detritus[i] <- max(vars$detritus[i - 1] + dDdt * dt, 0)
+
+    setTxtProgressBar(pb, i)
+  }
+
+  close(pb)
+
+  vars
+}
+
+# Run the models ---------------------------------------------------------
+
+length <- 400
+dt <- 0.05
+
+vars <- init_vars(length, dt)
+model_base <- run_model(dt, vars, sinking_rate_type = 0)
+
+vars <- init_vars(length, dt)
+model_sinking_1 <- run_model(dt, vars, sinking_rate_type = 1)
+
+vars <- init_vars(length, dt)
+model_sinking_2 <- run_model(dt, vars, sinking_rate_type = 2)
 
 # Plot results -----------------------------------------------------------
 
-# par(mfrow = c(3, 2))
+plot_model <- function(model) {
+  model |>
+    select(time, nutrients, zoo, phyto, detritus) |>
+    pivot_longer(nutrients:detritus, names_to = "variable", values_to = "value") |>
+    ggplot(aes(x = time, y = value, linetype = variable)) +
+    geom_line() +
+    scale_linetype_manual(values = state_vars_palette) +
+    scale_x_continuous(limits = c(0, max(time)), expand = c(0, 0)) +
+    scale_y_continuous(limits = c(0, 2)) +
+    labs(x = "NPZD", y = "t (days)", linetype = "Variable") +
+    theme_test()
+}
 
-# plot(vars$time, vars$theta, type = "l", col = "black")
-# plot(vars$time, vars$R, type = "l", col = "black")
-# plot(vars$time, vars$G, type = "l", col = "black")
+plot_model(model_base)
 
-# plot(vars$time, vars$nutrients, type = "l", col = "red", ylim = c(0, 2))
-# plot(vars$time, vars$zoo, type = "l", col = "blue")
-# plot(vars$time, vars$phyto, type = "l", col = "green")
-# plot(vars$time, vars$detritus, type = "l", col = "yellow")
+sinking_data <- data.frame(
+  time = model_sinking_1$time,
+  type_1 = model_sinking_1$sinking_rate,
+  type_2 = model_sinking_2$sinking_rate
+) |>
+  pivot_longer(2:3, names_to = "type", values_to = "value")
 
-# plot(vars$time, vars$L_Z, type = "l")
-# plot(vars$time, vars$L_P, type = "l")
-
-vars |>
-  select(time, nutrients, zoo, phyto, detritus) |>
-  pivot_longer(nutrients:detritus, names_to = "variable", values_to = "value") |>
-  ggplot(aes(x = time, y = value, color = variable)) +
+ggplot(sinking_data, aes(x = time, y = value, linetype = type)) +
   geom_line() +
-  theme_bw()
+  theme_test()
 
-# plot(vars$time, vars$L_PN, type = "l")
-# lines(vars$time, vars$L_PD, type = "l")
-# lines(vars$time, vars$L_ZN, type = "l")
-# lines(vars$time, vars$L_ZD, type = "l")
+plot_model(model_sinking_1)
